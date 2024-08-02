e942adb66d8dfd61319280f306cb92497b53967b
Ashu Pal <95235468+Ashu784@users.noreply.github.com>
2024-08-02 14:46:27 +0530

    Update Design Browser History.cpp

--- Design Browser History.cpp
+++ Design Browser History.cpp
@@ -1,32 +1,31 @@
-#include <bits/stdc++.h> 
-class Browser
-{
-    public:
+class BrowserHistory {
+public:
     vector<string>history;
     int pos;
-    Browser(string &homepage)
-    {
+    BrowserHistory(string homepage) {
         history.push_back(homepage);
-        pos = 0;
+        pos = 0;   
     }
-
-    void visit(string &url)
-    {
+    
+    void visit(string url) {
         history.erase(history.begin()+pos+1, history.end());
         history.push_back(url);
         pos++;
-
     }
-
-    string back(int steps)
-    {
-        pos = max(0, pos - steps);
+    
+    string back(int steps) {
+        if(steps < history.size())
+            pos = pos - steps;
+        else
+            pos = 0;
         return history[pos];
     }
-
-    string forward(int steps)
-    {
-        pos = min((int)history.size() - 1, pos + steps);
-        return history[pos];
+    
+    string forward(int steps) {
+        if((steps + pos) < history.size())
+            pos = pos + steps;
+        else
+            pos = history.size()-1;
+        return history[pos];      
     }
 };
